c012f3e3474a98b4ffc743315d85c98985ea0d45
nuhainul <9663525@gmail.com>
2024-04-01 12:30:54 +0600

    first commit: installed react, installed router, install firebase, created firebase.init config & nothing else

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC0ojnBqzG80oEu8jayBLADFR8IPgdWftY",
+  authDomain: "phm-49-simple-firebase.firebaseapp.com",
+  projectId: "phm-49-simple-firebase",
+  storageBucket: "phm-49-simple-firebase.appspot.com",
+  messagingSenderId: "423043270633",
+  appId: "1:423043270633:web:6706873c6ec335fcd12d78"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
